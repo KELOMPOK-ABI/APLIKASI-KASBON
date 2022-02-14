11e56eaaf3911cc074f7bde60ff0dc4a6ae35f75
Abiyyu Raihan <abiyyuraihan26@gmail.com>
2022-02-14 09:32:24 +0700

    abiyyu

--- SERVER/controller/KasbonController.js
+++ SERVER/controller/KasbonController.js
@@ -1,18 +1,83 @@
 class KasbonController {
-    static getKasbons(req,res){
+    static getKasbons(req, res) {
+        Item.findAll({
+            include: [ User ]
+        })
+            .then(result => {
+                res.json(result)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    }
+    static info(req, res) {
+        const id = +req.params.id;
 
+        Item.findByPk(id)
+            .then(result => {
+                res.json(result)
+            })
+            .catch(err => {
+                res.json(err)
+            })
     }
-    static info(req,res){
-        
+    static create(req, res) {
+        const { name, type, price, stock, image, UserId } = req.body;
+
+        Item.create({
+            name, type, price, stock, image, UserId
+        })
+            .then(result => {
+                res.json(result);
+            })
+            .catch(err => {
+                res.json(err)
+            })
     }
-    static create(req,res){
-        
+    static delete(req, res) {
+        const id = +req.params.id
+
+        Item.destroy({
+            where: { id }
+        })
+            .then(result => {
+                if (result === 1) {
+                    res.json({
+                        message: `Id ${id} deleted!`
+                    })
+                } else {
+                    res.json({
+                        message: `Id ${id} not deleted!`
+                    })
+                }
+            })
+            .catch(err => {
+                res.json(err)
+            })
     }
-    static delete(req,res){
-        
-    }
-    static update(req,res){
-        
+    static update(req, res) {
+        const id = +req.params.id
+        const { name, type, price, stock, image, UserId } = req.body
+
+        Item.update({
+            name, type, price, stock, image, UserId
+        }, {
+            where: { id }
+        })
+            .then(result => {
+                if(result[0] === 1){
+                    res.json({
+                        message: `Id ${id} has been updated!`
+                    })
+                }else{
+                    res.json({
+                        message: `Id ${id} has been not updated`
+                    })
+                }
+            })
+            .catch(err => {
+                res.json(err)
+            })
     }
 }
 
